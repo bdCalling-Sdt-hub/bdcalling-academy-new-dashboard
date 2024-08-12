a04010c9ecbbabf56d841067b92abb17ffef6284
shukumar-542 <shukumar542@gmail.com>
2024-08-12 14:05:05 +0600

    Added axios config image api

--- src/Pages/TrainerFeedback.jsx
+++ src/Pages/TrainerFeedback.jsx
@@ -4,22 +4,22 @@ import useGetRequest from '../Hooks/useGetRequest'
 const TrainerFeedback = () => {
 
     const [Loading, reviews, error,] = useGetRequest('review', `/show-student-feedback`)
-    console.log(reviews?.data?.data)
     return (
         <div>
             <p className='text-2xl font-semibold my-6'>Trainer All Feedback</p>
             <div className='grid grid-cols-3 gap-5'>
                 {
                     reviews?.data?.data?.map(item => {
+                        
                         return <div className='card-shadow p-4 rounded-md' key={item?.id}>
                             <div className='flex justify-start items-center gap-2'>
                                 {
-                                    item?.user?.image ? <img className='h-10 w-10 rounded-full' src={`${item?.user?.image}  `} alt="image" /> : <img className='h-10 w-10 rounded-full' src= "https://i.ibb.co/1GH871B/blank-profile-picture-973460-960-720.webp" alt="image" />
+                                    item?.user?.image ? <img className='h-10 w-10 rounded-full' src={`${imageUrl}/${item?.user?.image}  `} alt="image" /> : <img className='h-10 w-10 rounded-full' src= "https://i.ibb.co/1GH871B/blank-profile-picture-973460-960-720.webp" alt="image" />
                                 }
                                 
                                 <p>{item?.user?.name}</p>
                             </div>
-                            <p className='tracking-wide leading-7 mt-2 text-justify'>{item?.feedback}</p>
+                            <p className='tracking-wide leading-7 mt-2 '>{item?.feedback}</p>
                         </div>
                     })
                 }
