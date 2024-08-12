d775cbfcb95c77ebf699b55f9a4edffbcc3ceec4
shukumar-542 <shukumar542@gmail.com>
2024-08-12 17:09:40 +0600

    Adeed optional chaining for data slice table

--- src/Pages/TeacherDashBoard.jsx
+++ src/Pages/TeacherDashBoard.jsx
@@ -243,7 +243,7 @@ const TeacherDashBoard = () => {
                     <p className="section-title">My Classes Routing</p>
                     <Link className="text-[#2492EB]" to={`/teacher/class-routine`}>See All</Link>
                 </div>
-                <Table dataSource={routineData.slice(0,5)} pagination={false} columns={columns} />
+                <Table dataSource={routineData?.slice(0,5)} pagination={false} columns={columns} />
             </div>
             <div className="bg-white Student-List my-6 rounded-md">
                 <div className="between-center px-4">
@@ -251,7 +251,7 @@ const TeacherDashBoard = () => {
                 </div>
                 <Table
                     columns={studentColumns}
-                    dataSource={TableData?.slice(0,5) || []}
+                    dataSource={TableData?.slice[0,5] || []}
                     pagination={false}
                 />
             </div>
