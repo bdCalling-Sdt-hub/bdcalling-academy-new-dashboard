849db6fb655cbddd5b4db595a555f14e7ab09579
shukumar-542 <shukumar542@gmail.com>
2024-08-14 10:42:40 +0600

    Implement enrolled class api integration on overview page

--- src/Pages/StudentOverview.jsx
+++ src/Pages/StudentOverview.jsx
@@ -8,6 +8,7 @@ import { RxCross1 } from "react-icons/rx";
 import { Link } from "react-router-dom";
 import { FaPlay, FaStar } from "react-icons/fa";
 import notenrolled from '../assets/notenrolled.png'
+import useGetRequest from "../Hooks/useGetRequest";
 const overviewData = [
     {
         title: 'Completed Classes',
@@ -73,7 +74,23 @@ const data = [
     },
 
 ]
+
+
+
 const StudentOverview = () => {
+
+    const [requestingCourse, Course, CourseError,] = useGetRequest('all-course', `/enrolled-courses`)
+    const data = Course?.map(item => {
+
+        return {
+            name: item?.batch?.course?.course_name,
+            image: `${imageUrl}/${item?.batch?.image}`,
+            rating: 5,
+            id: item?.id
+        }
+    })
+
+
     return (
         <>
             <PageHeading text={`Overview`} />
@@ -86,41 +103,46 @@ const StudentOverview = () => {
                 </button>
             </div>
             <p className="text-4xl font-medium my-6">States in which you are enrolling</p>
-            <div className=" w-full my-6 bg-white card-shadow rounded-md grid grid-cols-4 justify-normal items-center gap-6 p-4 ">
-                {
-                    data?.map((item, i) => {
-                        return <div className="card-shadow w-full h-full" key={i}>
-                            <div className="w-full h-[230px] relative">
-                                <img src={item?.image} className="w-full h-full object-cover" alt="" />
-                                <button className="text-xl p-3 animate-pulse rounded-full bg-white text-blue-500 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                                    <FaPlay />
-                                </button>
-                            </div>
-                            <p className="text-[#0596FF] my-3 px-2 font-semibold text-lg">{item?.name}</p>
-                            <div className="h-[3px] bg-blue-500 w-full"> </div>
-                            <div className="between-center py-3 px-2">
-                                <p>{item?.percentage}% complete</p>
-                                <p className="flex justify-end items-center text-yellow-400 text-base gap-1"><FaStar className="-mt-1" /> {item?.rating} </p>
-                            </div>
-                            <div className="center-center mb-4 px-4">
-                                <button className="capitalize w-full py-2 px-6 bg-blue-500 rounded-md text-white">
-                                    Continue Course
-                                </button>
-                            </div>
-                        </div>
-                    })
-                }
-            </div>
-            <div>
-                <div className="center-center card-shadow flex-col mb-3 p-4">
-                    <img className="" src={notenrolled} alt="" />
-                    <p className="text-4xl font-medium my-6">You are not enrolled in any courses</p>
-                    <p>You are not enrolled in any course. Browse the course again</p>
-                    <button className="capitalize py-2 px-6 bg-blue-500 rounded-md text-white mt-2">
+            {
+                data?.length ?
+                    <div className=" w-full my-6 bg-white card-shadow rounded-md grid grid-cols-4 justify-normal items-center gap-6 p-4 ">
+                        {
+                            data?.map((item, i) => {
+                                return <div className="card-shadow w-full h-full" key={i}>
+                                    <div className="w-full h-[230px] relative">
+                                        <img src={item?.image} className="w-full h-full object-cover" alt="" />
+                                        <button className="text-xl p-3 animate-pulse rounded-full bg-white text-blue-500 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                                            <FaPlay />
+                                        </button>
+                                    </div>
+                                    <p className="text-[#0596FF] my-3 px-2 font-semibold text-lg">{item?.name}</p>
+                                    <div className="h-[3px] bg-blue-500 w-full"> </div>
+                                    <div className="between-center py-3 px-2">
+                                        {/* <p>{item?.percentage}% complete</p> */}
+                                        <p className="flex justify-end items-center text-yellow-400 text-base gap-1"><FaStar className="-mt-1" /> {item?.rating} </p>
+                                    </div>
+                                    <div className="center-center mb-4 px-4">
+                                        <button to={`/student/student-course/${item?.id}`} className="capitalize w-full py-2 px-6 bg-blue-500 rounded-md text-white">
+                                            Continue Course
+                                        </button>
+                                    </div>
+                                </div>
+                            })
+                        }
+                    </div> :
+                    <div>
+                        <div className="center-center card-shadow flex-col mb-3 p-4">
+                            <img className="" src={notenrolled} alt="" />
+                            <p className="text-4xl font-medium my-6">You are not enrolled in any courses</p>
+                            <p>You are not enrolled in any course. Browse the course again</p>
+                            {/* <button className="capitalize py-2 px-6 bg-blue-500 rounded-md text-white mt-2">
                         Browse courses
-                    </button>
-                </div>
-            </div>
+                    </button> */}
+                        </div>
+                    </div>
+
+            }
+
         </>
     )
 }
