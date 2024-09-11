f2ad4c0e0d85010c93243def2b9e779135f4a6d8
shaharulsiyam <shaharulsiyam0273@gmail.com>
2024-09-11 09:31:03 +0600

    change url

--- src/AxiosConfig/useAxiosConfig.jsx
+++ src/AxiosConfig/useAxiosConfig.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 const baseUrl = axios.create({
     // baseURL: 'http://192.168.10.64:7000/api',
-    baseURL: 'http://115.127.156.13:7000/api',
+    baseURL: 'https://mainserver.bdcallingacademy.com/api',
 })
 const useAxiosConfig = () => {
     return baseUrl
 }
-export const imageUrl = 'http://115.127.156.13:7000'
+export const imageUrl = 'https://mainserver.bdcallingacademy.com'
 // export const imageUrl = 'http://192.168.10.64:7000'
 export default useAxiosConfig
