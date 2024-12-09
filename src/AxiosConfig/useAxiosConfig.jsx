ca04c9b98cc3362644cada68717ec4e2ded44da3
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-12-09 11:00:55 +0600

    s

--- src/AxiosConfig/useAxiosConfig.jsx
+++ src/AxiosConfig/useAxiosConfig.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 const baseUrl = axios.create({
-    // baseURL: 'http://192.168.10.64:7000/api',
-    baseURL: 'https://mainserver.bdcallingacademy.com/api',
+    baseURL: 'http://165.232.137.110/api',
+    // baseURL: 'https://mainserver.bdcallingacademy.com/api',
 })
 const useAxiosConfig = () => {
     return baseUrl
 }
-export const imageUrl = 'https://mainserver.bdcallingacademy.com'
-// export const imageUrl = 'http://192.168.10.64:7000'
+// export const imageUrl = 'https://mainserver.bdcallingacademy.com'
+export const imageUrl = 'http://165.232.137.110'
 export default useAxiosConfig
